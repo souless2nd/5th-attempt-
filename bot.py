0bc638afd9882a6d32a19a174b78423fa1389e11
souless2nd <36045871+souless2nd@users.noreply.github.com>
2020-02-12 12:21:11 -0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+import datetime
+import emoji
+import os
+import random
+client = discord.Client()
+bot = commands.Bot(command_prefix = "!")
+bot.remove_command("help")
+@bot.event
+async def on_ready():
+    print("Im online")
+    Bot.wait_until_ready
+    client.wait_until_ready
+    game = discord.Game("with your feelings")
+    guild = bot.get_guild(547259564094128148)
+    await bot.change_presence(status=discord.Status.online, activity=game)
+extenstions = []
+if __name__ == '__main__':
+    for extension in extenstions:
+        try:
+            bot.load_extension(extension)
+            print(extension)
+        except Exception as error:
+            print(error)
+bot.run(os.environ['token'])
